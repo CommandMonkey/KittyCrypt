afffc91a385d6a9387c249757c68fb571ebc49be
Andreas <andafrana@gmail.com>
2024-02-02 10:34:40 +0100

    Gun accuracy while pivoting
    DET FUNKAR OMG AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/Scripts/GunRotation.cs
+++ Assets/Scripts/GunRotation.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GunRotation : MonoBehaviour
 {
+    [SerializeField] Transform barrelExitPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +16,24 @@ public class GunRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
+        Vector3 gunPivotToExit = transform.localPosition - transform.InverseTransformPoint(barrelExitPoint.position);
+        Vector3 gunPivotToMouse = transform.localPosition - transform.InverseTransformPoint(mousePos);
+
+        float angle = Mathf.Atan(gunPivotToExit.y / gunPivotToMouse.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+        
+
         Vector3 lookRotation = transform.position - mousePos;
-        transform.rotation = Quaternion.FromToRotation(Vector3.left, lookRotation);
+        Quaternion pivotToMouseRotation = Quaternion.FromToRotation(Vector3.left, lookRotation);
+        Vector3 eulerAngles = pivotToMouseRotation.eulerAngles;
+        eulerAngles.z -= angle;
+
+        transform.rotation = Quaternion.Euler(eulerAngles);
+
+
+
+
     }
 }
