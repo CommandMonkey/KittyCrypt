e582410ce51f12db5bfc6eabd21f52a2c36b82e8
Alinto <AntonLisborg@gmail.com>
2024-03-01 10:40:26 +0100

    Enemy Movement Fix

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -47,7 +47,7 @@ public class EnemyBehavior : MonoBehaviour
 
     void Update()
     {
-        if (shootingDistance == false )
+        if (shootingDistance == false)
         { MoveTowardsTarget(); }
 
         if (inMeleeRange == true)
@@ -106,12 +106,14 @@ public class EnemyBehavior : MonoBehaviour
 
         Debug.DrawLine(transform.position, hit.point, Color.blue, 0.1f);
 
-        if (hit.collider != null || hit.point == Vector2.zero)
+        if (hit.collider != null)
         {
-            lineOfSight = false ;
+            Debug.Log("Cant see player");
+            lineOfSight = false;
         }
         else
         {
+            Debug.Log("Can see player");
             lineOfSight = true;
             playerPosition = target.position;
         }
@@ -129,6 +131,7 @@ public class EnemyBehavior : MonoBehaviour
 
     void MoveTowardsTarget()
     {
+        if (playerPosition == Vector3.zero) return;
         // Calculate the direction from the current position to the target position
         Vector3 direction = playerPosition - base.transform.position;
 
