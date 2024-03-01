8321296bbe237cc90e894e7ad4dc6422d443ed16
Ivan <deez76322@gmail.com>
2024-03-01 09:11:45 +0100

    comict deez nuts >:C

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -12,19 +12,16 @@ using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 public class EnemyBehavior : MonoBehaviour
 {
 
-    //Me and the targets position  
-
+    //The targets position  
     public Transform target;
 
     //variables
-
     [SerializeField, Range(1, 10)] private float speed = 5f;
     [SerializeField, Range(1, 10)] private float distanceToTarget = 5f;
     [SerializeField, Range(1, 10)] private float meleeRange = 5f;
     [SerializeField, Range(1, 1000)] private float HP = 1f;
 
     //varibles
-
     private bool lineOfSight = true;
     private bool shootingDistance = false;
     private bool inMeleeRange = false;
@@ -34,19 +31,17 @@ public class EnemyBehavior : MonoBehaviour
     private Vector3 previousPosition;
 
     //LayerMasks
-
     public LayerMask obsticleLayer;
 
     //declerations
-
     SpriteRenderer spriteRenderer;
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidBody2D;
 
     void Start()
     {
         previousPosition = transform.position;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -63,7 +58,11 @@ public class EnemyBehavior : MonoBehaviour
         CheakWalkDirection();
         ShootLineOfSightRay();
         ShootMeleeRay();
-        
+    }
+
+    void TakeDamage(int damage)
+    {
+
     }
 
     void HitPlayer()
@@ -131,11 +130,7 @@ public class EnemyBehavior : MonoBehaviour
         // Normalize the direction vector to ensure consistent speed in all directions
         direction.Normalize();
 
-        // Move the object towards the target using Translate
-        //transform.Translate(direction * speed * Time.deltaTime);
-
-
-
-        rigidbody2D.velocity = direction * speed;
+        //Move myself
+        rigidBody2D.velocity = direction * speed;
     }
 }
