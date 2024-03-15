ce4b29654a9ef37e5babfd43d0be2ed458467d49
Alinto <AntonLisborg@gmail.com>
2024-03-15 10:39:48 +0100

    ESC MAKE GO STOP

---
+++ Assets/TimeManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenuContainer;
+
+    public bool gamePaused;
+
+    private void Awake()
+    {
+        pauseMenuContainer.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------
+    // Pausing
+    //-------------------------------------------------------------------------------------------------------------------
+
+    public void Pause()
+    {
+        if (!gamePaused) TogglePause();
+    }
+    public void UnPause()
+    {
+        if (gamePaused) TogglePause();
+    }
+
+    void TogglePause()
+    {
+        gamePaused = !gamePaused;
+
+        pauseMenuContainer.SetActive(gamePaused);
+
+        UpdateTimeScale();
+    }
+
+
+
+    /////////////////
+    // TimeScale
+    //
+
+    public void UpdateTimeScale()
+    {
+        Time.timeScale = (gamePaused) ? 0f : 1f;
+    }
+}
