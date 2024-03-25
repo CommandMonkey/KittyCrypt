3a4e693668e7fbdb01e9dc31c7beda9508ccfdd5
Alinto <AntonLisborg@gmail.com>
2024-03-25 17:50:59 +0100

    Saker händer inte men dom finns

--- Assets/Scripts/GameHelper.cs
+++ Assets/Scripts/GameHelper.cs
@@ -6,9 +6,12 @@ public class GameHelper
 {
     public static Vector2 GetRandomPosInCollider(BoxCollider2D collider)
     {
-        Vector2 pos = collider.transform.position - (Vector3)(collider.size / 2) + (Vector3)collider.offset;
-        pos.x = UnityEngine.Random.Range(pos.x, collider.size.x);
-        pos.y = UnityEngine.Random.Range(pos.y, collider.size.y);
+        Vector2 pos = (Vector2)collider.transform.position - (collider.size / 2) + collider.offset;
+
+        // Adjust position by half the size of the collider to ensure the position is within the bounds
+        pos.x += UnityEngine.Random.Range(0f, collider.size.x);
+        pos.y += UnityEngine.Random.Range(0f, collider.size.y);
+
         return pos;
     }
 }
