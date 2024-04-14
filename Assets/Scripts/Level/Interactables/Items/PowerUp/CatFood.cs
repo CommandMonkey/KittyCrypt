d84f45c438753532bdd337a655d5cd4e139ffe6e
Alinto <AntonLisborg@gmail.com>
2024-04-14 23:54:50 +0200

    oesjofnwg

--- Assets/Scripts/Level/Interactables/Items/PowerUp/CatFood.cs
+++ Assets/Scripts/Level/Interactables/Items/PowerUp/CatFood.cs
@@ -1,8 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CatFood : MonoBehaviour
 {
+    [SerializeField] GameObject pickupVFX;
 
+    Player player;
+
+    private void Start()
+    {
+        player = GameSession.Instance.player;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
 }
