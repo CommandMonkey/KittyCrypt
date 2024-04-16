3211dce0ede3200b2cc8cb4449472462b2d78a29
Jonatan Barth <Jonteba07@gmail.com>
2024-04-16 11:04:54 +0200

    vapen

---
+++ Assets/Explosion.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] float BlastRadius = 3f;
+    [SerializeField] float explotionDamage = 70f;
+    [SerializeField] LayerMask layers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ContactFilter2D contactFilter = new ContactFilter2D()
+        {
+            layerMask = layers
+        };
+
+        Collider2D[] collider2Ds = new Collider2D[20];
+        int colliderCount = Physics2D.OverlapCircle(transform.position, BlastRadius, contactFilter, collider2Ds);
+
+        if (colliderCount > 0)
+        {
+            foreach (Collider2D collider in collider2Ds) 
+            {
+               if (collider.CompareTag("Enemie"))
+                {
+                    collider.gameObject.GetComponent<Enemy>().TakeDamage(explotionDamage);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
