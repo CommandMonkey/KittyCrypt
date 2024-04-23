75f676eda838779775590f7b87984b50f55cfb94
Ivan <deez76322@gmail.com>
2024-04-23 08:31:27 +0200

    KAABOOMMM -tf2 DemoMan

--- Assets/Scripts/Interactables/Items/Weapon/Bullets/BouncingBullet.cs
+++ Assets/Scripts/Interactables/Items/Weapon/Bullets/BouncingBullet.cs
@@ -1,21 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BouncingBullet : Bullet
 {
+    float destroyAfter = 2;
+    private void Start()
+    {
+        Invoke("Explode", destroyAfter);
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         Enemy enemyScript = other.gameObject.GetComponent<Enemy>();
 
         if (enemyScript != null)
         {
-            Debug.Log("BOING");
             enemyScript.TakeDamage(damage);
-
-            var ffxInstance = Instantiate(hitEffect, transform.position, Quaternion.identity);
-            Destroy(ffxInstance, destroyHitEffectAfter);
-            Destroy(gameObject);
+            Explode();
         }
+    }
+
+    void Explode()
+    {
+        var ffxInstance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(ffxInstance, destroyHitEffectAfter);
+        Destroy(gameObject);
     }
 }
